9210fc72ed87d963f8edc5d53f202b744783993a
= <=>
2022-06-11 17:55:07 +0200

    changed sort funciton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 import { useState, useEffect, useCallback } from "react";
-import Movie from "./components/Movie";
+import Movie from "./components/Movie"; // component has name index.js, which can be ommited
 import { FaSortAmountDownAlt } from "react-icons/fa";
-import debounce from "lodash/debounce";
+import debounce from "lodash/debounce"; // I have imported debounce from lodash, altough the same effect could be achieved using useEffect and setTimeout
 import "./App.css";
 const { REACT_APP_API_URL, REACT_APP_API_KEY } = process.env;
+// common pattern is to store these values inside .env, which is usually in .gitignore, so every developer can have his own .env file with these credentials and keys
 
 function App() {
 	const [moviesIds, setMoviesIds] = useState([]);
 	const [movies, setMovies] = useState([]);
 	const [searchValue, setSearch] = useState("");
-	const [sorted, setSorted] = useState(false);
+	const [sorted, setSorted] = useState(false); // is not neccessary, but for future styling of the buttons
 
 	useEffect(() => {
 		axios
@@ -24,37 +25,44 @@ function App() {
 	}, []);
 
 	const getSearchResults = useCallback(
-			debounce((query) => {
-				axios
-					.get(`${REACT_APP_API_URL}/search/movie`, {
-						params: { api_key: REACT_APP_API_KEY, query },
-					})
-					.then((response) => {
-						setMovies(response.data.results);
-						setMoviesIds(
-							response.data.results.map((movie) => movie.id)
-						);
-					});
-			}, 300),
+		debounce((query) => {
+			axios
+				.get(`${REACT_APP_API_URL}/search/movie`, {
+					params: { api_key: REACT_APP_API_KEY, query },
+				})
+				.then((response) => {
+					setMovies(response.data.results);
+					setMoviesIds(
+						response.data.results.map((movie) => movie.id)
+					);
+				});
+			// if users types another character withing 300ms, this function will not run
+			// so user has to make a time gap between letters in order to load movies
+		}, 300),
 		[]
 	);
 	const search = (e) => {
-		setSearch(e.target.value);
-		getSearchResults(e.target.value);
+		setSearch(e.target.value); // changing the state
+		getSearchResults(e.target.value); // calling debounced functions
 	};
 
 	const sort = () => {
 		if (!sorted) {
 			setMovies((movies) =>
-				movies.sort((a, b) => a.vote_average - b.vote_average)
+				// sorts items by their vote_average decreasing => b-a
+        // increasing would be a-b
+				movies.sort((a, b) => b.vote_average - a.vote_average)
 			);
 		} else {
+			// copy of original movies stores only IDs in order to have web app fast if lots of movies are loaded(which doesn't happen in this example)
+			// sorts by position of movies the original array
 			setMovies((movies) =>
 				movies.sort(
-					(a, b) => moviesIds.indexOf(a.id) - moviesIds.indexOf(b.id)
+					(a, b) => moviesIds.indexOf(a.id) - moviesIds.indexOf(b.id) // comparing original positions of movies
 				)
 			);
 		}
+		// I am settings it after the sorting, because setting state is not completely synchronous and may result in later bugs
 		setSorted((s) => !s);
 	};
 	return (
@@ -62,7 +70,7 @@ function App() {
 			<input
 				className="search-input"
 				value={searchValue}
-				onChange={search}
+				onChange={search} // you don't have to use (e) => search(e) | e is the default
 				placeholder="Search a movie"
 			/>
 			<button className="btn-sort" onClick={sort}>
